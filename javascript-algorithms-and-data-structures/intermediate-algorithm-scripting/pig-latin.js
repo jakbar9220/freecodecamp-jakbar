3b3ced7a83942d942c3479487087b3955bacdfc6
jakbar9220 <47397555+jakbar9220@users.noreply.github.com>
2023-10-21 20:55:44 +0500

    Update and rename pig-latin.txt to pig-latin.js

--- javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/pig-latin.js
+++ javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/pig-latin.js
@@ -1,6 +1,6 @@
 function translatePigLatin(str) {
   let consonantCluster = str.match(/^[^aeiou]+/)
-  console.log(str.replace(consonantCluster,"").concat(consonantCluster).concat("ay"))
+  // console.log(str.replace(consonantCluster,"").concat(consonantCluster).concat("ay"))
   return consonantCluster!=null?str.replace(consonantCluster,"").concat(consonantCluster).concat("ay"):str.concat("way")
 }
 
